f1a323239fd0221d0b988717d403e4644a1d38b9
SoucaThomas <thomassouca@gmail.com>
2025-08-30 22:04:20 +0300

    throttler for upload endpoint

--- apps/server/src/upload/upload.module.ts
+++ apps/server/src/upload/upload.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 1 minute
+        limit: 10, // 10 requests per minute
+      },
+    ]),
+  ],
   controllers: [UploadController],
-  providers: [UploadService],
+  providers: [
+    UploadService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class UploadModule {}
